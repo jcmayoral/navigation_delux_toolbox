150f44ba0cebd0cf2572d031a2eefacf95f2f46d
Jose Mayoral <jocamaba1989@gmail.com>
2018-04-16 13:35:48 +0200

    exception avoided... launch files swapping and service stopped correctly

--- roslaunch_mode_switcher/ros/src/roslaunch_mode_switcher_ros/mode_manager.py
+++ roslaunch_mode_switcher/ros/src/roslaunch_mode_switcher_ros/mode_manager.py
@@ -2,10 +2,10 @@
 import rospy
 import yaml
 from roslaunch_mode_switcher_ros.mode_switcher import RosLaunchMode
-from roslaunch_mode_switcher.srv import ModeSwitcher
+from roslaunch_mode_switcher.srv import *
 
 class ModeManager:
-    def __init__(self,config_file):
+    def __init__(self,config_file,default):
         if rospy.has_param('~config_file'):
             config_file = rospy.get_param("~config_file")
         else:
@@ -16,7 +16,7 @@ class ModeManager:
         self.available_modes = dict()
         stream.close()
 
-        self.current_mode = None
+        self.current_mode = default
         self.is_stop_requested = False
 
         for key, value in modes.iteritems():
@@ -24,18 +24,24 @@ class ModeManager:
             self.available_modes[key] = RosLaunchMode(package = value['package'],
                                                       route_to_launch_file = value['path_to_file'])
 
+
         self.request_mode = rospy.Service('request_mode', ModeSwitcher, self.runService)
 
 
     def runService(self,req):
 
-        if req.request_mode is 'Stop':
+        resp = ModeSwitcherResponse()
+
+        print (req.request_mode.data, type(req.request_mode.data))
+        if req.request_mode.data == "Stop":
+            print "here"
             self.is_stop_requested = True
+            resp.succeeded.data = True
+            return resp
 
         if self.current_mode is not None:
             self.available_modes[self.current_mode].stop()
 
-        print req.request_mode
         self.available_modes[req.request_mode.data].start()
-
         self.current_mode = req.request_mode.data
+        return resp
